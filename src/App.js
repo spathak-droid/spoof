a539b46bd0efa292aa4dc71f24605eb7bea57bfd
spathak-droid <pathaksandesh025@gmail.com>
2023-03-28 15:44:20 -0500

    chnages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import sound from './Esters-Rd.mp3'
+
+const emoji = String.fromCodePoint(0x1F606);
+// U+1F606
+
+
 
 function App() {
+  const audio = new Audio(sound);
+
+  document.addEventListener("click", function() {
+   audio.play()
+  });
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      YOU JUST GOT TROLLED{emoji}
+      <div>
+        Click Anywhere Though.
+      </div>
     </div>
   );
 }
